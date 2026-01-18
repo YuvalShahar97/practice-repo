caf21aba7b49614dc48a3a7a0145a10fbf6f5173
shaked1turgeman <shaked1turgeman@gmail.com>
2026-01-18 21:31:27 +0200

    Add check_status function to app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+def check_status():
+    # פונקציה פשוטה שמחזירה True כדי שיהיה מה לבדוק בטסטים
+    return True
+
+if __name__ == "__main__":
+    print("Jenkins Project App is Running!")
+    print(f"Status: {check_status()}")
